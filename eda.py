c29584b55e44315d620643ac6cece259b187a357
Joe <joepbravo@gmail.com>
2025-11-24 07:05:46 -0800

    New complementary functions added to helper_funcs script and updated EDA script with new helper functions. And included init script.

--- eda.py
+++ eda.py
@@ -5,12 +5,17 @@ import os
 import pandas as pd
 pd.set_option('display.max_rows', 12)
 pd.set_option('display.max_columns', 10)
-from helper_funcs import query_tbl
+from helper_funcs import (
+    query_tbl
+    , extract_demand
+    , extract_supply
+    , transportation_costs
+)
 
 # Import table from PostgreSQL
 df = query_tbl()
 # Gathering just transportation costs
-trans_costs = df.iloc[:-1, 1:-1]
+trans_costs = transportation_costs(df)
 # Gathering some basic info
 num_of_plants = len(trans_costs.index)
 num_of_customers = len(trans_costs.columns)
@@ -21,31 +26,13 @@ sample15 = trans_costs.sample(
     axis = 1
 )
 sample15.to_csv('./data/sample15.csv', index=False)
-# print(sample15)
 
 # Next is the dataframe for customer demand
 # Extract the demand row into its own Pandas Series
-demand = df.iloc[-1,:]
-# Convert from series to dataframe
-demand = demand.to_frame(name = 'demand')
-# Drop NAs from the dataframe
-demand = demand.dropna()
-# Some rearranging
-demand = demand.iloc[1:, :]
-# Resetting index
-demand = demand.reset_index()
-# Drop unnecessary column that was the index
-demand = demand.drop(labels="index", axis=1)
+demand = extract_demand(df)
 
-# The last dataframe is the production capacity for the various factories
-# Pull the last column which is the production capacity
-prod_cap = df.iloc[:,-1]
-# Remove NA's
-prod_cap = prod_cap.dropna()
-# Turn data type from float to integer
-prod_cap = prod_cap.astype(int)
-# Convert from series to dataframe
-prod_cap = prod_cap.to_frame(name = 'production_capacity')
+# Supply
+prod_cap = extract_supply(df)
 
 # Combine the two dataframes
 eda_df = pd.concat(
--- helper_funcs.py
+++ helper_funcs.py
@@ -1,6 +1,5 @@
-import yaml # Read config files
 import time
-import pandas as pd
+import yaml # Read config files
 from sqlalchemy import create_engine
 
 def query_tbl(config_dir = "config.yaml"):
@@ -35,3 +34,74 @@ def query_tbl(config_dir = "config.yaml"):
     total_time = end_time - start_time # calculate the time
     print(f"Read time: {total_time:.3f} seconds") # print time
     return(df_table)
+
+# Transportation costs
+def transportation_costs(
+    df,
+    yield_df=True
+):
+    """Pull the data in the middle of the queried table that is just transportation costs."""
+    if yield_df:
+        return df.iloc[:-1, 1:-1]
+    else:
+        return df.iloc[:-1, 1:-1].to_numpy()
+
+# Pull demand
+def extract_demand(
+    df,
+    yield_df=True
+):
+    """
+    Pull the demand row, the final row in the queried table.
+    Can output either a Pandas series or NumPy array.
+
+    Args:
+        df (pandas.dataframe): Table queried from Postgres with transportation costs.
+        to_dataframe (bool): Determines whether function outputs a pandas dataframe or numpy array. Default is True.
+
+    Returns:
+        Customer demand in one vector (pandas.dataframe or numpy array).
+    """
+    if yield_df:
+        # Series of data processing logic that outputs a dataframe
+        demand = df.iloc[-1,:]
+        demand = demand.to_frame(name = 'demand')
+        demand = demand.dropna()
+        demand = demand.iloc[1:, :]
+        demand = demand.reset_index()
+        demand = demand.drop(labels="index", axis=1)
+    else:
+        # Convert Pandas dataframe to a NumPy series after data cleaning,
+        demand = df.iloc[-1, 1:]
+        demand = demand.reset_index(drop=True)
+        demand = demand.dropna()
+        demand = demand.astype(int)
+        demand = demand.to_numpy()
+    return demand
+
+# Get the supply
+def extract_supply(
+    df,
+    yield_df=True
+):
+    """
+    Extract the production capacity for each plant which is the last column.
+    Performing some data cleaning before deciding on array or dataframe.
+
+    Args:
+        df (pandas dataframe): Table queried from Postgres with transportation costs.
+        to_dataframe (bool): Determines whether function outputs a pandas dataframe or numpy array. Default is True.
+
+    Returns:
+        Production capacity one vector (pandas dataframe or numpy array).
+    """
+    prod_cap = df.iloc[:,-1]
+    prod_cap = prod_cap.dropna()
+    prod_cap = prod_cap.astype(int)
+    if yield_df:
+        # Convert from series to dataframe
+        prod_cap = prod_cap.to_frame(name = 'production_capacity')
+    else:
+        # Convert to numpy array
+        prod_cap.to_numpy()
+    return prod_cap
